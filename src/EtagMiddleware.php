d7d9ebd9a0cd73f5ef8e4a1354f6a0319c325e38
Miguel Piedrafita <github@miguelpiedrafita.com>
2018-04-11 16:46:22 +0200

    Create EtagMiddleware.php

---
+++ src/EtagMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace M1guelpf\Etag;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Middleware\ThrottleRequests as Middleware;
+
+class Etag extends Middleware
+{
+    /**
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @param  int|string               $maxAttempts
+     * @param  float|int                $decayMinutes
+     * @return mixed
+     * @throws \Illuminate\Http\Exceptions\ThrottleRequestsException
+     */
+    public function handle(Request $request, Closure $next, $maxAttempts = 60, $decayMinutes = 1)
+    {
+        if (! $request->isMethod('get')) {
+            return parent::handle($request, $next, $maxAttemps, $decayMinutes);
+        }
+        
+        $key = $this->resolveRequestSignature($request);
+        $maxAttempts = $this->resolveMaxAttempts($request, $maxAttempts);
+
+        $response = $next($request);
+
+        $response->setEtag($etag = md5($response->getContent()));
+        $requestEtag = str_replace('"', '', $request->getETags());
+
+        if ($requestEtag && $requestEtag[0] == $etag) {
+            return $this->addHeaders(
+                $response->setNotModified(), $maxAttempts,
+                $this->calculateRemainingAttempts($key, $maxAttempts)
+            );
+        }
+        
+        if ($this->limiter->tooManyAttempts($key, $maxAttempts)) {
+            throw $this->buildException($key, $maxAttempts);
+        }
+        
+        $this->limiter->hit($key, $decayMinutes);
+
+        return $this->addHeaders(
+            $response, $maxAttempts,
+            $this->calculateRemainingAttempts($key, $maxAttempts)
+        );
+    }
+}
